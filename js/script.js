6e3e69e085ecf19623e8493438d3b86c5bac4724
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-07-30 14:18:18 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// controllo per prima cosa se ho collegato bene il mio file
+console.log('Js ok');
+
+/*Traccia:
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine stampa in console nomecognomecolorepreferito21
+ES: marcolanciverde21 */
